4a402affae53539a79a4af578c267342620c97fd
zeyad3406 <76768774+zeyad3406@users.noreply.github.com>
2025-05-17 20:39:53 +0300

    Update Car_insurance.sql

--- Car_insurance.sql
+++ Car_insurance.sql
@@ -1,56 +1,55 @@
---problem a. Find the total number of people who owned cars that were involved in accidents in 2017.
-SELECT COUNT(DISTINCT Owns.customerId)
-FROM Owns, Accident
-WHERE Owns.CarId = Accident.CarId
-  AND Accident.Year = 2017;
+--problem A. Total number of people who owned cars involved in accidents in 2017
+SELECT COUNT(DISTINCT cars.customer_id)
+FROM cars, accidents
+WHERE cars.plate = accidents.car_plate
+  AND YEAR(accidents.date) = 2017;
 
---problem b. Find the number of accidents in which the cars belonging to Ahmed Mohamed were involved.
+--problem B. Number of accidents for cars owned by Ahmed Mohamed
 SELECT COUNT(*)
-FROM Accident, Owns, Customer
-WHERE Accident.CarId = Owns.CarId
-  AND Owns.customerId = Customer.customerId
-  AND Customer.first_Name = 'Ahmed'
-  AND Customer.last_Name = 'Mohamed';
-  --problem c. Find the model with maximum number of accidents in 2017.
-SELECT Car.Model
-FROM Car, Accident
-WHERE Car.CarId = Accident.CarId
-  AND Accident.Year = 2017
-GROUP BY Car.Model
+FROM accidents, cars, customers
+WHERE accidents.car_plate = cars.plate
+  AND cars.customer_id = customers.id
+  AND customers.name = 'Ahmed Mohamed';
+
+--problem C. Model with max accidents in 2017
+SELECT cars.model
+FROM cars, accidents
+WHERE cars.plate = accidents.car_plate
+  AND YEAR(accidents.date) = 2017
+GROUP BY cars.model
 HAVING COUNT(*) = (
   SELECT MAX(accident_count)
   FROM (
     SELECT COUNT(*) accident_count
-    FROM Car, Accident
-    WHERE Car.CarId = Accident.CarId
-      AND Accident.Year = 2017
-    GROUP BY Car.Model
-  )
+    FROM cars, accidents
+    WHERE cars.plate = accidents.car_plate
+      AND YEAR(accidents.date) = 2017
+    GROUP BY cars.model
+  ) AS sub
 );
 
---problem d. Car models with zero accidents
-SELECT M.Model_id, M.Name
-FROM Model M
-WHERE M.Model_id NOT IN (
-    SELECT DISTINCT C.Model_id
-    FROM Car C
-    JOIN Accident A ON C.Car_id = A.Car_id
+--problem D. Car models with zero accidents
+SELECT DISTINCT cars.model
+FROM cars
+WHERE cars.model NOT IN (
+    SELECT DISTINCT c.model
+    FROM cars c
+    JOIN accidents a ON c.plate = a.car_plate
 );
 
---problem e. All customers with cars involved in accidents in a certain year
+--problem E. Customers with cars in accidents in a certain year
 DECLARE @Year INT = 2024;
 
-SELECT DISTINCT CU.Customer_id, CU.Name, CU.Phone, CU.Address
-FROM Customer CU
-JOIN Customer_Car CC ON CU.Customer_id = CC.Customer_id
-JOIN Car C ON CC.Car_id = C.Car_id
-JOIN Accident A ON C.Car_id = A.Car_id
-WHERE YEAR(A.Date) = @Year;
+SELECT DISTINCT customers.id, customers.name, customers.phone
+FROM customers
+JOIN cars ON customers.id = cars.customer_id
+JOIN accidents ON cars.plate = accidents.car_plate
+WHERE YEAR(accidents.date) = @Year;
 
--- f. Number of accidents involving cars of a specific model
-DECLARE @ModelID INT = 101;
+--problem F. Number of accidents for a specific model
+DECLARE @Model VARCHAR(255) = 'Toyota Corolla';
 
 SELECT COUNT(*) AS Number_of_Accidents
-FROM Accident A
-JOIN Car C ON A.Car_id = C.Car_id
-WHERE C.Model_id = @ModelID;
+FROM accidents a
+JOIN cars c ON a.car_plate = c.plate
+WHERE c.model = @Model;
